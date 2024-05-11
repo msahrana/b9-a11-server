4cfe70666ef2ab0b5f0c9724be58c416aca8a064
msahrana <msahrana@gmail.com>
2024-05-12 00:15:05 +0600

    mongo db code added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 var cookieParser = require("cookie-parser");
-const {MongoClient, ServerApiVersion} = require("mongodb");
+const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -39,6 +39,18 @@ async function run() {
     app.get("/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {email: email};
     });
 
     console.log(
